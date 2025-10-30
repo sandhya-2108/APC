8c328cc22e80de084bf69600094cea05fc55578a
sandhya-2108 <sandhyac2108@gmail.com>
2025-10-30 15:50:08 +0530

    Add files via upload

---
+++ pandas/read_csv.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv("D:\python\pandas\hello.csv")
+print(df)
+# ser = pd.series(df['Name'])
+# print(ser)
+# ser = pd.DataFrame(df[['Name','Roll No.','Age']])
+# print(ser)
